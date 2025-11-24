79463f0550845071575e755b1759d08a4a0e86e1
marrcelolimaa <marceloliima.dev@gmail.com>
2025-11-24 18:14:13 -0300

    correção visual comando ping!

--- commands/ping.js
+++ commands/ping.js
@@ -12,22 +12,24 @@ module.exports = {
     async execute(message, args, client) {
         
         const messageLatency = Date.now() - message.createdTimestamp;
-
+        const emojiwifi = "<a:gatonet:1442614687551328276>"
+        const emojiponto = "<a:ponto:1442614730421440552>"
+        const emojicerto = "<a:serto:1442614527018401936>"
         const apiLatency = client.ws.ping;
 
         const pingEmbed = new EmbedBuilder()
             .setColor(0x9ACD32) // Verde claro
-            .setTitle('**Latência do Sistema**')
+            .setTitle(`**${emojicerto}  Latência do Sistema**`)
             .setDescription(`**Aqui estão as Latências atuais do Bot:**`)
             .setThumbnail(client.user.displayAvatarURL()) // Foto do bot
             .addFields(
                 { 
-                    name: '**Conexão (API)**', 
+                    name: `**${emojiwifi}  Conexão (API)**`, 
                     value: `**${apiLatency}ms**`, 
                     inline: true 
                 },
                 { 
-                    name: '**Processamento**', 
+                    name: `**${emojiponto}  Processamento**`, 
                     value: `**${messageLatency}ms**`, 
                     inline: false 
                 }
